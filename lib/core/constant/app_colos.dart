afe43876120e5e8349e37f219849441d36556acb
shinhyehwa <nguyenhongxd2002@gmail.com>
2024-03-30 10:03:34 +0700

    create base weather_app

---
+++ lib/core/constant/app_colos.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color blue = Color(0xff60B4E2);
+  static const Color dark = Color(0xff363B64);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xffE5E5E5);
+  static const Color red = Color(0xffee5253);
+  static const Color black = Color(0xff000000);
+  static const Color greyText = Color(0xffA098AE);
+}
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,96 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconGen {
+  const $AssetsIconGen();
+
+  /// File path: assets/icon/arrow_left.svg
+  String get arrowLeft => 'assets/icon/arrow_left.svg';
+
+  /// File path: assets/icon/location.svg
+  String get location => 'assets/icon/location.svg';
+
+  /// File path: assets/icon/search.svg
+  String get search => 'assets/icon/search.svg';
+
+  /// File path: assets/icon/sunny.svg
+  String get sunny => 'assets/icon/sunny.svg';
+
+  /// File path: assets/icon/temperature.svg
+  String get temperature => 'assets/icon/temperature.svg';
+
+  /// File path: assets/icon/weather-hail.svg
+  String get weatherHail => 'assets/icon/weather-hail.svg';
+
+  /// File path: assets/icon/wind.svg
+  String get wind => 'assets/icon/wind.svg';
+
+  /// List of all assets
+  List<String> get values =>
+      [arrowLeft, location, search, sunny, temperature, weatherHail, wind];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/clear.svg
+  String get clear => 'assets/images/clear.svg';
+
+  /// File path: assets/images/cloundy.svg
+  String get cloundy => 'assets/images/cloundy.svg';
+
+  /// File path: assets/images/fog.svg
+  String get fog => 'assets/images/fog.svg';
+
+  /// File path: assets/images/mostly_cloudy.svg
+  String get mostlyCloudy => 'assets/images/mostly_cloudy.svg';
+
+  /// File path: assets/images/mostly_rainny_sunny.svg
+  String get mostlyRainnySunny => 'assets/images/mostly_rainny_sunny.svg';
+
+  /// File path: assets/images/mostly_sunny.svg
+  String get mostlySunny => 'assets/images/mostly_sunny.svg';
+
+  /// File path: assets/images/rain.svg
+  String get rain => 'assets/images/rain.svg';
+
+  /// File path: assets/images/rainny.svg
+  String get rainny => 'assets/images/rainny.svg';
+
+  /// File path: assets/images/sleet.svg
+  String get sleet => 'assets/images/sleet.svg';
+
+  /// File path: assets/images/snow.svg
+  String get snow => 'assets/images/snow.svg';
+
+  /// File path: assets/images/storm.svg
+  String get storm => 'assets/images/storm.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        clear,
+        cloundy,
+        fog,
+        mostlyCloudy,
+        mostlyRainnySunny,
+        mostlySunny,
+        rain,
+        rainny,
+        sleet,
+        snow,
+        storm
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconGen icon = $AssetsIconGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
